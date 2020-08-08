e8465a237804a10c26cdd7a77e028fda1d1bd47e
Pat Faiola <pfaiola@gmail.com>
2020-08-08 14:09:19 -0400

    Adding Collection page and Collection Item

---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './preview-collection.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
+
+const PreviewCollection = ({ title, items }) => (
+    <div class="collection-preview">
+        <h1 class="title">{ title }</h1>
+        <div className="preview">
+            {
+                items.filter((item, index) => index < 4).map(item => (
+                    <CollectionItem key={item.id} {...item} />
+                ))
+            }
+        </div>
+    </div>
+)
+
+export default PreviewCollection;
