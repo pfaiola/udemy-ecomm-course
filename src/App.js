edb2eb7c25610bd46c18724558251f5b60c83e67
Pat Faiola <pfaiola@gmail.com>
2020-08-09 14:10:30 -0400

    Added redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -42,7 +42,7 @@ class App extends React.Component {
     <div>
       <Header></Header>
       <Switch>
-        <Route path="/signin" component={SignInAndSignUpPage}></Route>
+        <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to="/"></Redirect>) : (<SignInAndSignUpPage></SignInAndSignUpPage>)}></Route>
         <Route path="/shop" component={ShopPage}></Route>
 
         <Route exact path="/" component={HomePage}></Route>
@@ -53,8 +53,12 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
