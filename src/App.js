c0d1cafac633fb83ea742164bf8e53ba3468d565
Pat Faiola <pfaiola@gmail.com>
2020-08-09 13:48:35 -0400

    added redux and setCurrentUser logic

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import { setCurrentUser } from './redux/user/user.actions';
+
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
-export default class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
+class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
           });
         });
       } else {
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }      
     });
   }
@@ -46,7 +40,7 @@ export default class App extends React.Component {
   render () {
     return(
     <div>
-      <Header currentUser={this.state.currentUser}></Header>
+      <Header></Header>
       <Switch>
         <Route path="/signin" component={SignInAndSignUpPage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
@@ -59,3 +53,8 @@ export default class App extends React.Component {
   }
 }
 
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 export default class SignIn extends React.Component {
 
@@ -16,10 +16,17 @@ export default class SignIn extends React.Component {
         };
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
+        
+        const { email, password } = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: ''});
+        } catch (error) {
+            console.log(error);
+        }
 
-        this.setState({ email: '', password: ''});
     }
 
     handleChange = (event) => {
@@ -33,7 +40,7 @@ export default class SignIn extends React.Component {
                 <h2>I already have an account</h2>
                 <span> Sign in with your email and password</span>
 
-                <form>
+                <form onSubmit={this.handleSubmit}>
 
                     <FormInput 
                         name="email" 
